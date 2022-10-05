ae43966349aae047a6aae8660f221f827163ce56
ayodeji <hellodejisol@gmail.com>
2022-10-06 00:04:16 +0200

    Upgrade web component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,15 +7,15 @@
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
   <!-- Adyen JS from TEST environment (change to live for production)-->
-  <script src="https://checkoutshopper-test.adyen.com/checkoutshopper/sdk/5.16.0/adyen.js"
-    integrity="sha384-eOf0O1MTPGB1DQpr+Yha0MrmJruQb5S82+tuyo4sLiyfo1hgbf6W+fNfLjjU7Sks"
-    crossorigin="anonymous"></script>    
+  <script src="https://checkoutshopper-test.adyen.com/checkoutshopper/sdk/5.26.0/adyen.js"
+     integrity="sha384-lI43zM/9FpB4HKD0qDaUXy+Cfmxu+hHDwk+7XERAh/zYcQkm/oimV1Cmx7oZ3mL4"
+     crossorigin="anonymous"></script>
 
   <!-- Adyen CSS from TEST environment (change to live for production)-->
 	<link rel="stylesheet"
-	  href="https://checkoutshopper-test.adyen.com/checkoutshopper/sdk/5.16.0/adyen.css"
-		integrity="sha384-zy4t7axSdzHBMGqwJAynlv3eFVNiWw68LMf7vgKXxl2zZ6A8FlpucOoA/J//GBaQ"
-		crossorigin="anonymous">  
+     href="https://checkoutshopper-live.adyen.com/checkoutshopper/sdk/5.26.0/adyen.css"
+     integrity="sha384-EWxYZbuFOr+TBHe/ugu0v3NOulSLFDx8Diy1Mb2WJk1TNzTJJHAuwiwW3gq6btNx"
+     crossorigin="anonymous">
 
   <link rel="stylesheet" href="/css/app.css">
   <title>Checkout Demo</title>
