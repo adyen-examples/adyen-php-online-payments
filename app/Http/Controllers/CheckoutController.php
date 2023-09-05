1af9dfe3ce7ba3250269e7872d329847500139e9
Ayodeji Ogundare <63319309+ayodejidev@users.noreply.github.com>
2023-09-05 15:53:28 +0200

    Improve the webhook function

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -1,56 +1,65 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Adyen\Model\Checkout\CreateCheckoutSessionRequest;
 use Adyen\Model\Checkout\Amount;
 use Adyen\Model\Checkout\LineItem;
 use Illuminate\Http\Request;
 use App\Http\AdyenClient;
+use Illuminate\Support\Facades\Config;
 use Symfony\Component\Console\Output\ConsoleOutput;
 
 class CheckoutController extends Controller
 {
     protected $checkout;
 
-    function __construct(AdyenClient $checkout) {
+    public function __construct(AdyenClient $checkout)
+    {
         $this->checkout = $checkout->service;
     }
 
-    public function index(){
+    public function index()
+    {
         return view('pages.index');
     }
 
-    public function preview(Request $request){
+    public function preview(Request $request)
+    {
         $type = $request->type;
-        return view('pages.preview')->with('type', $type);
+        return view('pages.preview', ['type' => $type]);
     }
 
-    public function checkout(Request $request){
-        $data = array(
+    public function checkout(Request $request)
+    {
+        $data = [
             'type' => $request->type,
-            'clientKey' => env('ADYEN_CLIENT_KEY')
-        );
+            'clientKey' => Config::get('adyen.client_key'),
+        ];
 
-        return view('pages.payment')->with($data);
+        return view('pages.payment', $data);
     }
 
-    public function redirect(Request $request){
-        return view('pages.redirect')->with('clientKey', env('ADYEN_CLIENT_KEY'));
+    public function redirect(Request $request)
+    {
+        $data = ['clientKey' => Config::get('adyen.client_key')];
+        return view('pages.redirect', $data);
     }
 
-    // Result pages
-    public function result(Request $request){
+    public function result(Request $request)
+    {
         $type = $request->type;
-        return view('pages.result')->with('type', $type);
+        return view('pages.result', ['type' => $type]);
     }
 
     /* ################# API ENDPOINTS ###################### */
     // The API routes are exempted from app/Http/Middleware/VerifyCsrfToken.php
 
-    public function sessions(Request $request){
+    public function sessions(Request $request)
+    {
         $orderRef = uniqid();
 
-        /*Setting base url so demo works in gitpod.io*/
+        // Setting the base URL
         $baseURL = url()->previous();
         $baseURL = substr($baseURL, 0, -15);
 
@@ -61,42 +70,47 @@ class CheckoutController extends Controller
         $lineItem2 = new LineItem();
         $lineItem2->setQuantity(1)->setAmountIncludingTax(5000)->setDescription("Headphones");
 
-        // Creating actual session request
+        // Creating the actual session request
         $sessionRequest = new CreateCheckoutSessionRequest();
         $sessionRequest
-        ->setChannel("Web")
-        ->setAmount($amount)
-        ->setCountryCode("NL")
-        ->setMerchantAccount(env('ADYEN_MERCHANT_ACCOUNT'))
-        ->setReference($orderRef)
-        ->setReturnUrl("${baseURL}/redirect?orderRef=${orderRef}")
-        ->setLineItems([$lineItem1, $lineItem2]);
+            ->setChannel("Web")
+            ->setAmount($amount)
+            ->setCountryCode("NL")
+            ->setMerchantAccount(Config::get('adyen.merchant_account'))
+            ->setReference($orderRef)
+            ->setReturnUrl("${baseURL}/redirect?orderRef=${orderRef}")
+            ->setLineItems([$lineItem1, $lineItem2]);
 
-        return $this->checkout->sessions($sessionRequest);}
+        return $this->checkout->sessions($sessionRequest);
+    }
+
     // Webhook integration
-    public function webhooks(Request $request){
-
-        $hmac_key = env('ADYEN_HMAC_KEY');
-        $validator = new \Adyen\Util\HmacSignature;
-
+    public function webhooks(Request $request)
+    {
+        $hmacKey = env('ADYEN_HMAC_KEY');
+        $validator = new \Adyen\Util\HmacSignature();
         $out = new ConsoleOutput();
-        
+
         $notifications = $request->getContent();
         // Add null handling
-
         $notifications = json_decode($notifications, true);
-        $notificationItems = $notifications['notificationItems'];
 
-        // fetch first (and only) NotificationRequestItem
-        $item = array_shift($notificationItems);
-        $requestItem = $item['NotificationRequestItem'];
+        if (isset($notifications['notificationItems'])) {
+            $notificationItems = $notifications['notificationItems'];
 
-        if ($validator->isValidNotificationHMAC($hmac_key, $requestItem)) {
-            // consume event asynchronously
-            // ie INSERT into DB or queue
-            $out->writeln("Eventcode " . json_encode($requestItem['eventCode'], true));
-        } else {
-            return response('[refused]', 401);
+            // Fetch the first (and only) NotificationRequestItem
+            $item = array_shift($notificationItems);
+
+            if (isset($item['NotificationRequestItem'])) {
+                $requestItem = $item['NotificationRequestItem'];
+
+                if ($validator->isValidNotificationHMAC($hmacKey, $requestItem)) {
+                    // Consume the event asynchronously (e.g., INSERT into DB or queue)
+                    $out->writeln("Eventcode " . json_encode($requestItem['eventCode'], true));
+                } else {
+                    return response('[refused]', 401);
+                }
+            }
         }
 
         return response('[accepted]', 200);
