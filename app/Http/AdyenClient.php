3b7dd161f2a7c2a8799c9d9956aaeb6433dc164c
Kwok He Chu <105217051+Kwok-he-Chu@users.noreply.github.com>
2026-06-16 16:05:36 +0200

    Fix ApplicationName

--- app/Http/AdyenClient.php
+++ app/Http/AdyenClient.php
@@ -10,7 +10,8 @@ class AdyenClient
         $client = new \Adyen\Client();
         $client->setXApiKey(env('ADYEN_API_KEY'));
         $client->setEnvironment(\Adyen\Environment::TEST);
-
+        $client->setApplicationName('[adyen-php-online-payments checkout-example adyen-web/5.68.0]');
+        
         $this->service = new \Adyen\Service\Checkout\PaymentsApi($client);
     }
 }
